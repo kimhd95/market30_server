b31c464a3d4ce18174f3b534a01394c22b27e9a9
김현동 <kimhd10@naver.com>
2019-06-27 22:53:38 +0900

    거리순가게목록 로드, 결제, 상품삭제 API 구현

--- market30_server/server/controllers/apis/users/index.js
+++ market30_server/server/controllers/apis/users/index.js
@@ -16,15 +16,17 @@ router.get('/images/:imageName', function(req, res) {
   let imageName = req.params.imageName;
   res.sendFile(`/home/ubuntu/market30_server/market30_server/server/public/${imageName}.jpg`);
 });
-
-// router.post('/register_buyer', APIService.registerBuyer);
-// router.post('/register_seller', APIService.registerSeller);
-// router.post('/register_store', APIService.registerStore);
-// router.post('/register_product', APIService.registerProduct);
 router.post('/get_buyer_info', APIService.getBuyerInfo);
+router.post('/register_buyer', APIService.registerBuyer);
+router.post('/register_seller', APIService.registerSeller);
+router.post('/register_store', APIService.registerStore);
 router.post('/register_product', APIService.registerProduct);
-router.post('/get_product_list', APIService.getProductList);
+router.post('/get_product_list_seller', APIService.getProductListSeller);
+router.post('/get_product_list_buyer', APIService.getProductListBuyer)
 router.post('/verify_barcode', APIService.verifyBarcode);
+router.post('/delete_product', APIService.deleteProduct);
+router.post('/make_payment', APIService.makePayment);
+router.post('/get_near_store', APIService.getNearStore);
 
 // ^Middleware. Make sure to put all the routes which needs authentication below this middleware.
 
--- market30_server/server/services/users/api_service.js
+++ market30_server/server/services/users/api_service.js
@@ -8,7 +8,6 @@ const cheerio = require('cheerio');
 
 function getBuyerInfo(req, res) {
   const {user_id, password} = req.body;
-
   models.sequelize.query(`SELECT * FROM buyers;`).then(result => {
     console.log(result[0]);
     if (result[0].length > 0) {
@@ -62,9 +61,37 @@ function registerProduct(req, res) {
   });
 }
 
-function getProductList(req, res) {
-  const seller_id = req.body.seller_id;
+function deleteProduct(req, res) {
+  const {seller_id, id, name} = req.body;
+  models.Product.count({
+    where: {
+      id: id,
+      seller_id: seller_id
+    }
+  }).then(result => {
+    if(result > 0) {
+      models.sequelize.query(`DELETE FROM products WHERE id=${id}`)
+      .then(result => {
+        return res.status(200).json({success: true});
+      })
+    }
+  }).catch(err => {
+    return res.status(500).json({success: false, message: "Internal Server or DB error."})
+  });
+}
 
+function getProductListBuyer(req, res) {
+  const query = `SELECT * FROM products`;
+  models.sequelize.query(query)
+  .then(result => {
+    return res.status(200).json({success: true, data: result[0]});
+  }).catch(err => {
+    return res.status(500).json({success: false, message: "Internal Server or DB error."})
+  });
+}
+
+function getProductListSeller(req, res) {
+  const seller_id = req.body.seller_id;
   models.sequelize.query(`SELECT * FROM products WHERE seller_id=${seller_id};`)
   .then(result => {
     if (result[0].length > 0) {
@@ -79,7 +106,6 @@ function getProductList(req, res) {
 
 function verifyBarcode(req, res) {
   const barcode = req.body.barcode;
-
   request.get({uri:`https://www.beepscan.com/barcode/${barcode}`}, function (error, response, body) {
     const $ = cheerio.load(body);
     const product_name = $("div.container b").text();
@@ -88,12 +114,78 @@ function verifyBarcode(req, res) {
   });
 }
 
+function makePayment(req, res) {
+  const {buyer_id, seller_id, product_id, price, count} = req.body;
+  const query = `INSERT INTO transactions (buyer_id, seller_id, product_id, price, count)
+   VALUES('${buyer_id}', '${seller_id}', ${product_id}, ${price}, ${count})`;
+
+  models.sequelize.query(query).then(result => {
+    const query_select = `SELECT * FROM products WHERE product_id=${product_id}`;
+    models.sequelize.query(query_select).then(result => {
+      if (result[0].length > 0) {
+        let new_count = result[0][0].count - count;
+        new_count = (new_count < 0) ? 0 : new_count;
+        const query_update = `UPDATE products SET count=${new_count} WHERE product_id=${product_id}`;
+        models.sequelize.query(query_update).then(result => {
+          return res.status(200).json({success: true, message: "Payment succeed."});
+        })
+      } else {
+        return res.status(403).json({success: false, message: "Critical Error: No Product_id matched."});
+      }
+    })
+  }).catch(err => {
+    return res.status(500).json({success: false, message: "Internal Server or DB Error."});
+  });
+}
+
+// 거리순 가게 리스트 리턴
+function getNearStore(req, res) {
+  const {ulat, ulng} = req.body;
+  const query = `SELECT * FROM stores`;
+
+  models.sequelize.query(query).then(result => {
+    const storeList = [];
+    const calFunction = function (item) {
+      const p = 0.017453292519943295; // Math.PI / 180
+      const c = Math.cos;
+      let a = 0.5 - c((item.lat - lat) * p) / 2
+              + c(lat * p) * c(item.lat * p)
+              * (1 - c((item.lng - lng) * p)) / 2;
+      let result = 12742 * Math.asin(Math.sqrt(a));
+      item['distance'] = Math.floor(result*1000) + 'm';
+      storeList.push(item);
+      return new Promise(resolve => setTimeout(() => resolve("ok"), 100));
+    }
+
+    var actions = result[0].map(calFunction);
+    Promise.all(actions).then(() => {
+      // 거리순 정렬
+      storeList.sort(function(a, b) {
+        return (a.distance < b.distance) ? -1 : (a.distance > b.distance) ? 0 : 1;
+      });
+      return res.status(200).json({success: true, data: storeList});
+    }).catch(err => {
+      return res.status(500).json({success: false, message: 'Internal Server or DB error.'});
+    });
+  }).catch(err => {
+    return res.status(500).json({success: false, message: 'Internal Server or DB error.'});
+  });
+}
+
+// 가게 검색
+
+
+
 module.exports = {
   getBuyerInfo: getBuyerInfo,
-  // registerBuyer: registerBuyer,
-  // registerSeller: registerSeller,
-  // registerStore: registerStore,
+  registerBuyer: registerBuyer,
+  registerSeller: registerSeller,
+  registerStore: registerStore,
   registerProduct: registerProduct,
-  getProductList: getProductList,
-  verifyBarcode: verifyBarcode
+  getProductListBuyer: getProductListBuyer,
+  getProductListSeller: getProductListSeller,
+  verifyBarcode: verifyBarcode,
+  deleteProduct: deleteProduct,
+  makePayment: makePayment,
+  getNearStore: getNearStore
 }
